c24016ca048a6ae3bdc2158a65861587cb79545f
Mike Lock <michael.lockwitz@consensys.net>
2019-08-27 15:41:46 -0600

    Added drizzle

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,63 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Heading, Box, Flex, Button, Card, Text, Link, Image } from "rimble-ui";
 
-function App() {
+function App(props) {
+  const [loading, setLoading] = useState(true);
+  const [drizzleState, setDrizzleState] = useState(null);
+
+  useEffect(() => {
+    const drizzle = props.drizzle;
+
+    // subscribe to changes in the store
+    const unsubscribe = drizzle.store.subscribe(() => {
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        setLoading(false);
+        setDrizzleState(drizzleState);
+      }
+    });
+
+    return function cleanup() {
+      unsubscribe();
+    };
+  });
+
   return (
     <Box className="App">
       <Flex justifyContent={"flex-end"} p={3}>
         <Button>Connect</Button>
       </Flex>
-      <Box maxWidth={"1180px"} p={3} mx={"auto"}>
-        <Heading.h1>Buy Tickets</Heading.h1>
-
-        <Text my={4} />
-        <Flex justifyContent={"space-between"}>
-          <Card width={"auto"}>
-            <Flex
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              flexDirection={"column"}
-            >
-              <Heading.h3 mb={3}>Attendance</Heading.h3>
-              <Image height={"200px"} width={"200px"} mb={3} />
-              <Button>Buy</Button>
-            </Flex>
-          </Card>
-
-          <Card width={"auto"}>
-            <Flex
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              flexDirection={"column"}
-            >
-              <Heading.h3 mb={3}>Food</Heading.h3>
-              <Image height={"200px"} width={"200px"} mb={3} />
-              <Button>Buy</Button>
-            </Flex>
-          </Card>
-
-          <Card width={"auto"}>
-            <Flex
-              alignItems={"center"}
-              justifyContent={"space-between"}
-              flexDirection={"column"}
-            >
-              <Heading.h3 mb={3}>Parties</Heading.h3>
-              <Image height={"200px"} width={"200px"} mb={3} />
-              <Button>Buy</Button>
-            </Flex>
-          </Card>
+      {loading ? (
+        <Flex justifyContent={"center"}>
+          <Heading.h3>Loading...</Heading.h3>
         </Flex>
-        <Heading.h4 mt={4} mb={2}>
-          About
-        </Heading.h4>
+      ) : (
+        <Box maxWidth={"1180px"} p={3} mx={"auto"}>
+          <Heading.h1>Buy Tickets</Heading.h1>
 
-        <Text mb={3} />
+          <Text my={4} />
+          <Flex justifyContent={"space-between"}>
+            <Card width={"auto"}>
+              <Flex
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDirection={"column"}
+              >
+                <Heading.h3 mb={3}>Attendance</Heading.h3>
+                <Image height={"200px"} width={"200px"} mb={3} />
+                <Button>Buy</Button>
+              </Flex>
+            </Card>
 
-        <Link href="https://rimble.consensys.design" target="_blank">
-          Learn more about Rimble
-        </Link>
-      </Box>
+            <Card width={"auto"}>
+              <Flex
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDirection={"column"}
+              >
+                <Heading.h3 mb={3}>Food</Heading.h3>
+                <Image height={"200px"} width={"200px"} mb={3} />
+                <Button>Buy</Button>
+              </Flex>
+            </Card>
+
+            <Card width={"auto"}>
+              <Flex
+                alignItems={"center"}
+                justifyContent={"space-between"}
+                flexDirection={"column"}
+              >
+                <Heading.h3 mb={3}>Parties</Heading.h3>
+                <Image height={"200px"} width={"200px"} mb={3} />
+                <Button>Buy</Button>
+              </Flex>
+            </Card>
+          </Flex>
+          <Heading.h4 mt={4} mb={2}>
+            About
+          </Heading.h4>
+
+          <Text mb={3} />
+
+          <Link href="https://rimble.consensys.design" target="_blank">
+            Learn more about Rimble
+          </Link>
+        </Box>
+      )}
     </Box>
   );
 }
--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import drizzle functions and contract artifact
+import { Drizzle } from "drizzle";
+import RimbleToken from "./contracts/RimbleToken.json";
+
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [RimbleToken],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545"
+    }
+  }
+};
+
+// setup drizzle
+const drizzle = new Drizzle(options);
+
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
