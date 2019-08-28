e40160f115337dd5ab1d703c85e3651c242a7681
Mike Lock <michael.lockwitz@consensys.net>
2019-08-28 16:14:34 -0600

    Changed name of token. Migrated to Rinkeby

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,24 +1,27 @@
-var Metadata = artifacts.require('./Metadata.sol')
-var RimbleToken = artifacts.require('./RimbleToken.sol')
+var Metadata = artifacts.require("./Metadata.sol");
+var RimbleToken = artifacts.require("./RimbleToken.sol");
 
-let _ = '        '
+let _ = "        ";
 
 module.exports = (deployer, helper, accounts) => {
-
   deployer.then(async () => {
     try {
       // Deploy Metadata.sol
-      await deployer.deploy(Metadata)
-      let metadata = await Metadata.deployed()
-      console.log(_ + 'Metadata deployed at: ' + metadata.address)
+      await deployer.deploy(Metadata);
+      let metadata = await Metadata.deployed();
+      console.log(_ + "Metadata deployed at: " + metadata.address);
 
-     // Deploy RimbleToken.sol
-      await deployer.deploy(RimbleToken, 'RimbleToken', 'RMBL', metadata.address)
-      let rimbleToken = await RimbleToken.deployed()
-      console.log(_ + 'RimbleToken deployed at: ' + rimbleToken.address)
-
+      // Deploy RimbleToken.sol
+      await deployer.deploy(
+        RimbleToken,
+        "DevCon VI Ticket",
+        "DC6A",
+        metadata.address
+      );
+      let rimbleToken = await RimbleToken.deployed();
+      console.log(_ + "RimbleToken deployed at: " + rimbleToken.address);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
-}
+  });
+};
