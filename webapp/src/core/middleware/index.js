d6c261d36a17083c28b489f2cdf674fa77a53f64
Mike Lock <michael.lockwitz@consensys.net>
2019-09-17 07:53:39 -0600

    Started adding reducers and actions. Not fully set up.

--- webapp/src/core/middleware/index.js
+++ webapp/src/core/middleware/index.js
@@ -1,3 +1,4 @@
+import { put, takeEvery } from "redux-saga/effects";
 import { generateStore } from "@drizzle/store";
 import drizzleOptions from "../../drizzleOptions";
 
@@ -7,6 +8,7 @@ const contractEventNotifier = store => next => action => {
 
   // tx started but not confirmed or rejects
   if (action.type === "PUSH_TO_TXSTACK") {
+    // write dispatch to show the started tx modal
   }
 
   // Show progress alert
@@ -36,13 +38,69 @@ const contractEventNotifier = store => next => action => {
   // tx is successful
   if (action.type === "TX_SUCCESSFUL") {
   }
+
+  if (action.type === "SHOW_CONFIRMATION_MODAL") {
+  }
   return next(action);
 };
 
+// const initialRimble = {
+//   rimble: {
+//     showConfirmation: false
+//   }
+// };
+
+// const updateTransactionView = (state = initialRimble, action) => {
+//   switch (action.type) {
+//     case "TOGGLE_CONFIRMATION":
+//       return {
+//         ...state,
+//         rimble: {
+//           showConfirmation: !state.rimble.confirmation
+//         }
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+// actions
+const TODOS_FETCH = "MY_APP/TODOS_FETCH";
+const TODOS_RECEIVED = "MY_APP/TODOS_RECEIVED";
+
+// reducers
+const todosReducer = (state = [], action) => {
+  if (action.type === TODOS_RECEIVED) {
+    // update your state
+    return action.todos;
+  }
+  return state;
+};
+
+// fetch data from service using sagas
+function* fetchTodos() {
+  const todos = yield fetch("https://jsonplaceholder.typicode.com/todos").then(
+    resp => resp.json()
+  );
+  yield put({ type: TODOS_RECEIVED, todos });
+}
+
+// Combine all your redux concerns
+
+// app root saga
+function* appRootSaga() {
+  yield takeEvery(TODOS_FETCH, fetchTodos);
+}
+
+// app Reducers and Sagas and Middlewares
+const appReducers = { todos: todosReducer };
+const appSagas = [appRootSaga];
 const appMiddlewares = [contractEventNotifier];
 
 export default generateStore({
   drizzleOptions,
+  appReducers,
+  appSagas,
   appMiddlewares,
   disableReduxDevTools: false
 });
