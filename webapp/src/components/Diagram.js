d90fbdf9c136a243adb88f977961a8c97b2f233c
Mike Lock <michael.lockwitz@consensys.net>
2019-09-25 10:10:38 -0600

    Added download diagram link.

--- webapp/src/components/Diagram.js
+++ webapp/src/components/Diagram.js
@@ -2,9 +2,10 @@ import React from "react";
 import RainbowBox from "./RainbowBox";
 import { drizzleConnect } from "@drizzle/react-plugin";
 import { toggleTxSuccessModal } from "../core/redux/actions";
-import { Heading, Box, Flex, Button, Pill, Image, Card } from "rimble-ui";
+import { Box, Flex, Image, Card, Link } from "rimble-ui";
 import diagramHeading from "./../images/rimble-ethereum-transaction-flow-headers.png";
 import diagramSwimlanes from "./../images/rimble-ethereum-transaction-flow-swimlanes.png";
+import fullDiagram from "./../images/rimble-ethereum-transaction-flow-swimlane-diagram.png";
 
 function Diagram({ setRoute, toggleTxSuccessModal }) {
   return (
@@ -20,6 +21,18 @@ function Diagram({ setRoute, toggleTxSuccessModal }) {
             <Image src={diagramHeading} />
             <Box height={"50vh"} overflow={"scroll"}>
               <Image src={diagramSwimlanes} />
+              <Flex justifyContent={"flex-end"} m={3}>
+                <Link
+                  href={fullDiagram}
+                  target={"_blank"}
+                  fontSize={0}
+                  title={
+                    "Download Rimble's Transaction Ethereum Lifecycle Diagram"
+                  }
+                >
+                  Download Rimble's Transaction Ethereum Lifecycle Diagram
+                </Link>
+              </Flex>
             </Box>
           </Box>
         </Card>
